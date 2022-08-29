6bbb5057e4a4581355204e60fc31e84bc256acfb
raamiiChu <87169493+raamiiChu@users.noreply.github.com>
2022-08-29 23:28:24 +0800

    模組6版
    新增確定對話框

--- 期末專題/basic_mod.py
+++ 期末專題/basic_mod.py
@@ -11,10 +11,12 @@ import pickle    #導入pickle模組
 #list4=[]跟誰
 #dict1={}行程:[日期]
 #dict2={}日期:[行程]
-#ct1={}日期:[行程]
-#ct2={}日期:[行程]
-#ct3={}日期:[行程]
-#ct4={}日期:[行程]
+
+#種類字典
+#ct1={}日期:[工作行程]
+#ct2={}日期:[學業行程]
+#ct3={}日期:[娛樂行程]
+#ct4={}日期:[其他行程]
 
 '''檢查用
     print(dict1)
@@ -29,21 +31,13 @@ import pickle    #導入pickle模組
     print(ct4)
 '''
 
-dict3={'0101':'元旦',\
-       '0228':'和平紀念日','0401':'愚人節','0501':'勞動節',\
-       '0808':'父親節','0928':'教師節','1010':'國慶日',\
-       '1031':'萬聖節','1225':'行憲紀念日','0308':'婦女節','0329':'青年節',\
-       '1025':'臺灣光復節','1112':'國父誕辰紀念日'}#日期:節日
+dict3={'0101':'元旦','0228':'和平紀念日','0401':'愚人節','0501':'勞動節',\
+       '0808':'父親節','0928':'教師節','1010':'國慶日','1031':'萬聖節',\
+       '1225':'行憲紀念日','0308':'婦女節','0329':'青年節',\
+       '1025':'臺灣光復節','1112':'國父誕辰紀念日'}#日期:國立節日
+
 dict4={'工作':'ct1', '學業':'ct2', '娛樂':'ct3', '其他':'ct4'}#行程類別
 
-'''
-'0226':'元宵節''0211':'除夕','0212':'春節開始','0216':'春節結束','0404':'清明節+兒童節',
-'0509':'母親節','0614':'端午節','0921':'中秋節','0301':'和平記念日補假',
-'0402':'兒童節補假','0430':'勞動節補假','0822':'中元節','0920':'中秋節補假',
-'1011':'國慶日補假','1014':'重陽節','1221':'冬至','1231':'元旦補假',
-'0621':'夏至',
-
-'''
 def inport():#讀取檔案
     try:#檢查全部檔案是否存在(可讀取)
         #建立字典、串列
@@ -81,6 +75,7 @@ def inport():#讀取檔案
         dict1=dict2=ct1=ct2=ct3=ct4={}
         list1=list2=list3=list4=[]
         export()
+        inport()
     
 def export():#輸出檔案
     with open('dict1.pickle', 'wb') as file:
@@ -108,6 +103,17 @@ def export():#輸出檔案
 def print_dict2date(x):#打印訊息用的
     for i in dict2[x]:
         print('\t'*2,i)
+
+def check_dict3(date):#判斷是否為節日
+    ax=str(date.month) #屬性:月
+    ay=str(date.day)   #屬性:天
+    if len(str(date.month))==1: #若月為個位數，前方補'0' ex.1 -> 01
+        ax='0'+str(date.month)
+    if len(str(date.day))==1:   #若日為個位數，前方補'0' ex.9 -> 09
+        ay='0'+str(date.day)
+    if ax+ay in dict3:          #若該日期有在字典中，因出對應節日
+        print(dict3[ax+ay])
+        
 
 #判定輸入的日期是否正確，之後要判斷日期可以直接用這個函數(date=check_date())
 def check_date():
@@ -138,12 +144,10 @@ def check_date():
             break
     return datetime.date(int(year),int(month),int(day))#回傳日期
 
-
 def write():#寫入
-    date=check_date()
-    if date in dict3:#判斷是否為節日
-        n=dict3[date]
-        print(n)
+    date=check_date() #判定輸入的日期是否正確
+    print('='*50)
+    check_dict3(date) #將日期帶入，判斷是否為節日
     print(date,'行程:',dict2.get(date,'無'))
     print('='*50)
     thing=str(input("請寫入您要記錄的事項:"))
@@ -156,7 +160,6 @@ def write():#寫入
             category=str(input("請重新輸入您的行程種類，1=工作, 2=學業, 3=娛樂, 4=其他："))
             
     who=str(input("請寫入此事項的關係人，若沒有請直接輸入Enter:"))#人
-    
     
     if category=="1":
         try:
@@ -200,10 +203,10 @@ def write():#寫入
              category = "娛樂"
         elif category == "4" :
             category = "其他"
-        list1.append(thing)#在行程串列中增加行程
-        list2.append(date)
-        list3.append(category)
-        list4.append(who)
+        list1.append(thing)     #在串列中增加行程
+        list2.append(date)      #在串列中增加日期
+        list3.append(category)  #在串列中增加種類
+        list4.append(who)       #在串列中增加關係人
         print('='*50)
         print('已將行程排入')
         print(date,':')
@@ -212,13 +215,12 @@ def write():#寫入
 def search():#查詢
     print('='*60)
     asksearch=input('輸入要查詢的項目(0:行程,1:日期,2:種類)')
-    while asksearch!='0' and asksearch!='1' and asksearch!='2':
+    while asksearch!='0' and asksearch!='1' and asksearch!='2': #檢查機制
         asksearch=input('輸入錯誤(0:行程,1:日期,2:種類)?')
-    if asksearch=='0':
+    if asksearch=='0': #查詢該行程在哪一天
         search=str(input('輸入要查詢的行程:'))
         print('='*60)
-        #字典不能用count，所以改用串列
-        if list1.count(search)>=1:#檢查串列中是否有該行程
+        if list1.count(search)>=1: #檢查串列中是否有該行程
             people=[i for i,x in enumerate(list1) if x==search]
             
             for i in people :
@@ -230,12 +232,10 @@ def search():#查詢
                 print("-"*30)    
         else:
             print(search,': 查無此行程')
-    elif asksearch=='1':
-        date=check_date()
+    elif asksearch=='1':  #查詢當天有哪些行程
+        date=check_date() #判定輸入的日期是否正確
         print('='*60)
-        if date in dict3:#判斷是否為節日
-            n=dict3[date]
-            print(n)
+        check_dict3(date) #將日期帶入，判斷是否為節日
         try:
             dict2[date]
         except:
@@ -245,20 +245,21 @@ def search():#查詢
                 print(date,':','無行程')
             else:
                 print(date,':')
-                print_dict2date(date)#將日期帶入函數
+                print_dict2date(date) #將日期帶入函數，印出訊息
     else:
         sel=str(input("請輸入您查詢的行程種類，1=工作, 2=學業, 3=娛樂, 4=其他："))
         while True:
-            if sel=="1" or sel=="2" or sel=="3" or sel=="4":
+            if sel=="1" or sel=="2" or sel=="3" or sel=="4": #檢查機制
                 break
             else:
                 sel=str(input("請重新輸入查詢的行程種類，1=工作, 2=學業, 3=娛樂, 4=其他："))
-        if sel=="1" and len(ct1)>=1:#檢查串列中是否有該行程
-            for date,thing in ct1.items():
-                if thing==[]:
+        if sel=="1" and len(ct1)>=1:
+            for date,thing in ct1.items(): #日期、行程
+                if thing==[]: #避免空串列
                     continue
                 else:
                     print(date,'的行程：',', '.join(thing))
+                    #印出該種類的所有對應行程
         elif sel=="2" and len(ct2)>=1:
             for date,thing in ct2.items():
                 if thing==[]:
@@ -281,10 +282,8 @@ def search():#查詢
             print('無行程')
 
 def rewrite():#修改
-    date=check_date()
-    if date in dict3:#判斷是否為節日
-        n=dict3[date]
-        print(n)
+    date=check_date() #判定輸入的日期是否正確
+    check_dict3(date) #將日期帶入，判斷是否為節日
     print('當日行程:',dict2.get(date,'查無資料'))
     try:
         dict2[date]
@@ -320,34 +319,34 @@ def rewrite():#修改
             print('取消修改')
 
 def delete():#刪除
-    date=check_date()
-    if date in dict3:#判斷是否為節日
-        n=dict3[date]
-        print(n)
+    date=check_date() #判定輸入的日期是否正確
+    print('='*60)
+    check_dict3(date) #將日期帶入，判斷是否為節日
     print('當日行程:',dict2.get(date,'查無資料'))
     try:
-        dict2[date]
+        dict2[date] #檢查當天是否有行程
     except:
-        print('無資料可刪除')
+        print('無資料可刪除') #沒有則直接結束
     else:
-        askdel=input('是否要刪除(0:是,1:否)?')
+        askdel=input('是否刪除(0:是,1:否)?') #詢問是否要刪除
         while askdel!='0' and askdel!='1':
             askdel=input('輸入錯誤(0:是,1:否)?')
-        if askdel=='0':
+        if askdel=='0': #確定刪除
             askdel_2=input('刪除數量(0:全部,1:一個)?')
             while askdel_2!='0' and askdel_2!='1':
                 askdel_2=input('輸入錯誤(0:全部,1:一個)?')
             if askdel_2=='0': #刪除全部
                 for x in dict2[date]:
+                                             #利用索引值刪除
                     del list2[list1.index(x)]#刪除串列中的日期
                     del list3[list1.index(x)]#刪除串列中的種類
                     del list4[list1.index(x)]#刪除串列中的跟誰
                     del list1[list1.index(x)]#刪除串列中的行程
-                                             #一定要在最後刪除
-                                             #否則前三行會因找不到索引值而刪除失敗
+                    #list1一定要在最後刪除，否則前三行會因找不到索引值而刪除失敗
                 for y in dict2[date]: #移除行程對應的日期(可能會留下空串列)
                     dict1[y].remove(date)
-                for x in dict2[date]: 
+                for x in dict2[date]: #刪除種類字典的對應值
+                                      #當天可能有多個行程，故用for迴圈
                     while True:
                         try:
                             del ct1[date] #嘗試是否能刪除，不論成功與否皆結束迴圈
@@ -376,13 +375,13 @@ def delete():#刪除
                             break
                         else:
                             break
-                del dict2[date]#刪除當日所有行程
+                del dict2[date] #刪除當日所有行程
                    
             else: #刪除一個
                 while True:
                     delete=str(input('請選擇你要刪除的事項:'))
                     try:
-                        dict2[date].remove(delete)
+                        dict2[date].remove(delete) #嘗試是否可移除
                         dict1[delete].remove(date)        
                     except:
                         print('查無此行程')
@@ -391,23 +390,24 @@ def delete():#刪除
                         #尋找該行程是哪個種類
                         if dict4.get(list3[list1.index(delete)])=='ct1':
                             del ct1[date][ct1[date].index(delete)]
+                            #刪除字典中的行程串列的對應索引值
                         elif dict4.get(list3[list1.index(delete)])=='ct2':
                             del ct2[date][ct2[date].index(delete)]
                         elif dict4.get(list3[list1.index(delete)])=='ct3':
                             del ct3[date][ct3[date].index(delete)]
                         elif dict4.get(list3[list1.index(delete)])=='ct4':
-                            del ct4[date][ct4[date].index(delete)]   
+                            del ct4[date][ct4[date].index(delete)]
+                                                      #利用索引值刪除
                         del list2[list1.index(delete)]#刪除串列中的日期
                         del list3[list1.index(delete)]#刪除串列中的種類
                         del list4[list1.index(delete)]#刪除串列中的跟誰
                         del list1[list1.index(delete)]#刪除串列中的行程
-                                                      #一定要在最後刪除
-                                                      #否則前三行會因找不到索引值而刪除失敗
+                        #list1一定要在最後刪除，否則前三行會因找不到索引值而刪除失敗
                         print('='*60)
                         print('刪除成功')
                         print(date,':')
-                        print_dict2date(date)#將日期帶入函數
+                        print_dict2date(date) #將日期帶入函數
                         break
-        else:
+        else: #取消刪除
             print('取消刪除')
     
