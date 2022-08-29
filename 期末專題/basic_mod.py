473250a6384d41126b48c01adfa78f3652057b0e
raamiiChu <87169493+raamiiChu@users.noreply.github.com>
2022-08-29 23:14:19 +0800

    模組2版
    新增 datetime 模組，並優化日期輸入

--- 期末專題/basic_mod.py
+++ 期末專題/basic_mod.py
@@ -1,36 +1,50 @@
+import datetime
 list1=[]#行程
 dict1={}#行程:[日期]
 dict2={}#日期:[行程]
-dict3={'0101':'元旦','0211':'除夕','0212':'春節開始','0216':'春節結束','0226':'元宵節',\
-       '0228':'和平紀念日','0401':'愚人節','0404':'清明節+兒童節','0501':'勞動節','0509':'母親節',\
-       '0614':'端午節','0808':'父親節','0921':'中秋節','0928':'教師節','1010':'國慶日',\
-       '1031':'萬聖節','1225':'行憲紀念日','0301':'和平記念日補假','0308':'婦女節','0329':'青年節',\
-       '0402':'兒童節補假','0430':'勞動節補假','0822':'中元節','0920':'中秋節補假','1011':'國慶日補假',\
-       '1014':'重陽節','1025':'臺灣光復節','1112':'國父誕辰紀念日','1221':'冬至','1231':'元旦補假','0621':'夏至',}#日期:節日
-'''print(list1) #這三行可用於檢查
-        print(dict1)
-        print(dict2)'''
+dict3={'0101':'元旦',\
+       '0228':'和平紀念日','0401':'愚人節','0501':'勞動節',\
+       '0808':'父親節','0928':'教師節','1010':'國慶日',\
+       '1031':'萬聖節','1225':'行憲紀念日','0308':'婦女節','0329':'青年節',\
+       '1025':'臺灣光復節','1112':'國父誕辰紀念日'}#日期:節日
+
+'''
+'0226':'元宵節''0211':'除夕','0212':'春節開始','0216':'春節結束','0404':'清明節+兒童節',
+'0509':'母親節','0614':'端午節','0921':'中秋節','0301':'和平記念日補假',
+'0402':'兒童節補假','0430':'勞動節補假','0822':'中元節','0920':'中秋節補假',
+'1011':'國慶日補假','1014':'重陽節','1221':'冬至','1231':'元旦補假',
+'0621':'夏至',
+'''
+'''
+print(list1) #這三行可用於檢查
+print(dict1)
+print(dict2)
+
+'''
+def print_dict2date(x):
+    for i in dict2[x]:
+        print('\t'*2,i)
 #判定輸入的資料是否正確，之後要判斷日期可以直接用這個函數(date=check_date())
 def check_date():
     print('='*60)
-    date=input("請選擇要編輯的日期(格式:月份日期,XXOO):")
+    year=input("請輸入要編輯的年份:")
+    while not year.isdigit():           #判斷輸入的年份是否為純數字
+        year=input("輸入錯誤，請輸入要編輯的年份:")
+    month=input("請輸入要編輯的月份:")  
+    while not month.isdigit() or int(month)>12 or int(month)<1:
+        #判斷輸入的月份是否為1~12
+        month=input("輸入錯誤，請輸入要編輯的月份:")
+    day=input("請輸入要編輯的日子:")
     while True:
-        if len(date)!=4 or (not date.isdigit()):
-            date=input('請輸入四位數的日期:')
-        elif int(date[0:2])<1 or int(date[0:2])>12:
-            date=input('不存在的日期，請重新輸入:')
-        elif (date[0:2]=='01' or date[0:2]=='03' or date[0:2]=='05'\
-            or date[0:2]=='07'or date[0:2]=='08' or date[0:2]=='10'\
-            or date[0:2]=='12') and (int(date[2:4])>31 or int(date[2:4])<1):
-            date=input('不存在的日期，請重新輸入:')
-        elif (date[0:2]=='04' or date[0:2]=='06' or date[0:2]=='09'\
-            or date[0:2]=='11') and (int(date[2:4])>30 or int(date[2:4])<1):
-            date=input('不存在的日期，請重新輸入:')
-        elif date[0:2]=='02' and (int(date[2:4])>29 or int(date[2:4])<1):
-            date=input('不存在的日期，請重新輸入:')
+        try:                            
+            datetime.date(int(year),int(month),int(day))
+            #判斷輸入的日子是否符合曆法
+        except:
+            day=input("輸入錯誤，請輸入要編輯的日子:")
+            continue
         else:
             break
-    return date
+    return datetime.date(int(year),int(month),int(day))#回傳日期
 
 
 def write():
@@ -38,7 +52,7 @@ def write():
     if date in dict3:#判斷是否為節日
         n=dict3[date]
         print(n)
-    print('當日行程:',dict2.get(date,'無'))
+    print(date,'行程:',dict2.get(date,'無'))
     print('='*50)
     thing=str(input("請寫入您要記錄的事項:"))
     try:
@@ -56,8 +70,14 @@ def write():
         dict1[thing].append(date)
     finally:
         list1.append(thing)#在行程串列中增加行程
-        print('已將行程排入',dict2.get(date))
-        
+        print('='*50)
+        print('已將行程排入')
+        print(date,':')
+        print_dict2date(date)#將日期帶入函數
+        print(list1) #這三行可用於檢查
+        print(dict1)
+        print(dict2)
+    
 
 def search():#查詢
     print('='*60)
@@ -66,15 +86,17 @@ def search():#查詢
         asksearch=input('輸入錯誤(0:行程,1:日期)?')
     if asksearch=='0':
         search=str(input('輸入要查詢的行程:'))
+        print('='*60)
         #字典不能用count，所以改用串列
         if list1.count(search)>=1:#檢查串列中是否有該行程
             print(search,':')
             for date in dict1[search]:
-                print('\t',date)
+                print('\t'*2,date)
         else:
             print(search,': 查無此行程')
     else:
         date=check_date()
+        print('='*60)
         if date in dict3:#判斷是否為節日
             n=dict3[date]
             print(n)
@@ -83,12 +105,11 @@ def search():#查詢
         except:
             print(date,':','無行程')
         else:
-            if int(len(dict2[date]))==0:
+            if int(len(dict2[date]))==0:  #避免有行程被刪除導致的空串列
                 print(date,':','無行程')
             else:
                 print(date,':')
-                for thing in dict2[date]:
-                    print('\t',thing)
+                print_dict2date(date)#將日期帶入函數
 
 def rewrite():
     date=check_date()
@@ -123,9 +144,11 @@ def rewrite():
                 else:
                     dict1[rewrite_thing].append(date) #加在dict1串列最後方
                 print('修改成功')
+                print('='*60)
+                print(date,':')
+                print_dict2date(date)#將日期帶入函數
         else:
             print('取消修改')
-
 
 def delete():
     date=check_date()
@@ -152,16 +175,21 @@ def delete():
                     dict1[y].remove(date)
                 del dict2[date]#刪除當日所有行程
             else: #刪除一個
-                delete=str(input('請選擇你要刪除的事項:'))
-                try:
-                    dict2[date].remove(delete)
-                    dict1[delete].remove(date)
-                    del list1[list1.index(delete)]
-                except:
-                    print('查無此行程')
-                else:
-                    print('刪除成功')
-                    print('刪除後的行程:',dict2[date])
+                while True:
+                    delete=str(input('請選擇你要刪除的事項:'))
+                    try:
+                        dict2[date].remove(delete)
+                        dict1[delete].remove(date)
+                        del list1[list1.index(delete)]
+                    except:
+                        print('查無此行程')
+                        continue
+                    else:
+                        print('='*60)
+                        print('刪除成功')
+                        print(date,':')
+                        print_dict2date(date)#將日期帶入函數
+                        break
         else:
             print('取消刪除')
 
